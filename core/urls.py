0b5a82a86546d9f3e4c802301f6167e4940d5568
zkao012 <zhaojianfeng2025@163.com>
2026-03-18 23:38:47 +0000

    Add login and register routes

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/tasks/'), name='home'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
     path('tasks/', views.task_list, name='task_list'),
     path('tasks/create/', views.task_create, name='task_create'),
     path('tasks/<int:pk>/delete/', views.task_delete, name='task_delete'),
