b38019bbf3ba3ae5d31151c06b775769deac6b5a
liveforsea <jieqihuang66@gmail.com>
2026-03-19 10:53:33 +0800

    Fix static files storage

--- trackit/settings.py
+++ trackit/settings.py
@@ -91,7 +91,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
